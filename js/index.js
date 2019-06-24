21bf99b28435f3727961f53336acfb93a32ee67c
Richard Lee <dlackty@gmail.com>
2019-06-24 16:59:45 +0800

    Fix up layout and tweak schedule

--- js/index.js
+++ js/index.js
@@ -44,14 +44,14 @@ $(document).ready(function() {
 function swiperSpeakers(type) {
     if(type == "left") tempID = sId - 1;
     if(type == "right") tempID = sId + 1;
-    if( tempID > 0 && tempID <= 3 ) {
+    if( tempID > 0 && tempID <= 2 ) {
         $('#speakers-'+sId).fadeOut(0);
         $('#speakers-'+tempID).fadeIn(0);
         $('.arrow-l, .arrow-r').fadeIn(0);
         sId = tempID;
     }
     if(sId == 1) {$('.arrow-l').fadeOut(0)}
-    if(sId == 3) {$('.arrow-r').fadeOut(0)}
+    if(sId == 2) {$('.arrow-r').fadeOut(0)}
 }
 
 function sendtoline(){
